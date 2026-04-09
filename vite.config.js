35a633f57526881f944c87754dc37425f659aaf8
Aditia Eka <aditiaeka.engineer@gmail.com>
2026-04-09 23:49:06 +0700

    Tugas 2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    plugins: [
-        laravel(["resources/css/app.css", "resources/js/app.js"]),
-        tailwindcss(),
-    ],
+    plugins: [laravel(["resources/css/app.css", "resources/js/app.js"])],
 });
